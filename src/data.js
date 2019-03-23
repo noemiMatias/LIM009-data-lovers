5b2387a56f2ad243674a8f753c6b829c2b36d0de
magalyelguera <elguera.magaly@gmail.com>
2019-03-22 19:56:32 -0500

    cominando obj

--- src/data.js
+++ src/data.js
@@ -1,31 +1,16 @@
-<<<<<<< HEAD
-console.log(WORLDBANK.PER.indicators[12].data.2002);
-=======
- /*console.log(WORLDBANK.PER.indicators[12].data);*/
- /*const example = () => {*/
-    const imprimir= document.getElementById('container');
-    const obj = {};
-    
-    const data=WORLDBANK.PER.indicators[12].data;
-    
-    const anios= Object.keys(data)
-    //console.log(anios);
-    for(let i=0; i<=anios.length-1 ;i++){
-      if (anios[i]>=2002){
-            obj[anios[i]]=data[anios[i]]
-            
-            imprimir.innerHTML = imprimir.innerHTML + `<p>${anios[i]} : ${data[anios[i]]}</p>`
-            /*obj[anios[i]] = data[anios[i]]*/
-        }
-    };
-    const filtrado = () => {
->>>>>>> 8db4768e29ed9a4db193fe7bf3773c3ce98648f3
 
-    }
-    console.log(filtrado);
+console.log(WORLDBANK.PER.indicators[12].data);
 
-    /*console.log(obj)*/
-  
 
-    /*console.log(obj);*/
-  //window.example = example;
+
+
+/* Manejo de data */
+
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
+
+const example = () => {
+  return 'example';
+};
+
+window.example = example
