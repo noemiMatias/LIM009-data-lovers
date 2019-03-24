0ef0d6fd84926ac583f289e42415f3165b6d283b
magalyelguera <elguera.magaly@gmail.com>
2019-03-24 14:56:14 -0500

    actualizado

--- src/data.js
+++ src/data.js
@@ -1,16 +1,50 @@
+ /*console.log(WORLDBANK.PER.indicators[12].data);*/
+ /*const example = () => {*/
+  const imprimir= document.getElementById('container');
+  const obj = {};
 
-console.log(WORLDBANK.PER.indicators[12].data);
+  const data=WORLDBANK.PER.indicators[12].data;
 
+  const anios= Object.keys(data)
+  //console.log(anios);
+  for(let i=0; i<=anios.length-1 ;i++){
+    if (anios[i]>=2002){
+          obj[anios[i]]=data[anios[i]]
 
-
-
-/* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
+          /*imprimir.innerHTML = imprimir.innerHTML + `<p>${anios[i]} : ${data[anios[i]]}</p>`*/
+          /*obj[anios[i]] = data[anios[i]]*/
+      }
+  };
+  const filtrado = () => {
+/*console.log(WORLDBANK.PER.indicators[12].data);*/
 const example = () => {
-  return 'example';
-};
+const arr = [];
+const data = WORLDBANK.PER.indicators[12].data;
 
-window.example = example
+const anios = Object.keys(data)
+for (let i = 0; i <= anios.length - 1; i++) {
+  if (anios[i] >= 2002) {
+    const obj = {};
+    obj.year = anios[i];
+    obj.value = data[anios[i]];
+    arr.push(obj)
+  }
+  console.log(filtrado);
+};
+return arr;
+};
+  /*console.log(obj)*/
+
+  /*console.log(obj);*/
+//window.example = example; 
+  }
+  /*SEGUNDO INDICE*/
+  /*console.log(WORLDBANK.PER.indicators[10].data);*/
+
+  const data10 = WORLDBANK.PER.indicators[10].data;
+  const anios10 = obj.keys(data);
+  for(let i=0; i<=anios10.length-1 ;i++){
+    if (anios10[i]>=2002){
+          obj[anios10[i]]=data[anio10[i]]
+    };
+  }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 /* Manejo del DOM */
 
+//window.example(); 
 //window.example();
+const container=document.getElementById("contenedor");
+const selectYears=document.getElementById("items");
+
+selectYears.addEventListener('change',()=>{
+    console.log(selectYears.value)
+    /*console.log(window.example())*/
+}) 
