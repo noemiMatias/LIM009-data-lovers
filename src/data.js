df2958c7b308a61cbc6b77988c072f9a2a24acc2
magalyelguera <elguera.magaly@gmail.com>
2019-03-31 15:47:57 -0500

    hemos logrado imprimir en el select los años que corresponden a cada indice

--- src/data.js
+++ src/data.js
@@ -1,36 +1,78 @@
 const todoPeruIndicadores = WORLDBANK['PER']['indicators'];
 
-// ARRAY QUE CONTIENE ID Y NAME DE LOS INDICADORES que queremos PUSHEAdos de LA FUNCION DE ABAJO 
+// ARRAY QUE CONTIENE ID Y NAME DE LOS INDICADORES QUE TENEMOS PUSHEADOS DE LA FUNCION DE CAPTURAR INDICE 
 
-
-// 1RO FUNCION QUE CAPTURE LOS INDICADORES POR CODIGO Y NAME QUE QUIERO (FUERZA LABORAL 6)
+// 1RA FUNCION QUE CAPTURA LOS INDICADORES POR CODIGO Y NAME QUE QUIERO (FUERZA LABORAL 6 INDICADORES)
 const capturarIndicadores = (data) => {
-  let arr= [""];
+  // ARRAY DE INDICADORES
+  let arr = [""];
   //  console.log(arr);
-  for( let i=0; i<data.length; i++){
-      if (i>9 && i<16){  
-    let obj ={
-    id:data[i].indicatorCode,
-   name:data[i].indicatorName
-      // console.log(valorIndicadores[i].id);
-    // console.log(valorIndicadores[i].name);
+  for (let i = 0; i < data.length; i++) {
+    if (i > 9 && i < 16) {
+      let obj = {
+        id: data[i].indicatorCode,
+        name: data[i].indicatorName
+        // console.log(valorIndicadores[i].id);
+        // console.log(valorIndicadores[i].name);
+      }
+      arr.push(obj);
+    }
   }
-  arr.push(obj);
-  
-}
-} 
-  
   return arr;
-  
-  };
-// FUNCION PARA OBTENER LOS AÑOS Y SUS VALORES
+};
 
- 
- 
+// EXPORTANDO FUNCION QUE NOS IMPRIME
+window.capturarIndicadores = capturarIndicadores;
 
+// 2DA FUNCION QUE CAPTURA  LOS AÑOS Y SUS VALORES
+// ARRAY CON INDICATORCODE Y TODOS LOS AÑOS
+var data2 = [];
+// console.log(data2);
+const capturarAnios = (dataValoresAnios) => {
+  // console.log(dataValoresAnios);
+  const arrayAnios = [];
+  // console.log(arrayAnios);
+  console.log(dataValoresAnios)
+  for (let i = 0; i < dataValoresAnios.length; i++) {
 
-         
-   
-    // EXPORTANDO FUNCION QUE NOS IMPRIME
-  window.capturarIndicadores= capturarIndicadores;
+    if (i > 9 && i < 16) {
+      const aniosTotal = dataValoresAnios[i].data;
 
+      const objAnios = {
+        data: dataValoresAnios[i].data,
+        indicatorCode: dataValoresAnios[i].indicatorCode
+      }
+      arrayAnios.push(objAnios);
+    }
+  }
+  // ESTAMOS SACANDO EL ARRAY DE LA FUNCION PARA PODER UTILIZARLO COMO ARGUMENTO 
+  data2 = arrayAnios;
+};
+
+// ESTO SE EJECUTA SOLO UNA VEZ PARA LLENAR EL NUEVO ARREGLO DATA2
+capturarAnios(todoPeruIndicadores);
+
+//LISTA DE AÑOS POR INDICADOR
+const listaAnio = (data,indicatorCode) => {
+  const arrAnio =[];
+  // console.log(arrAnio);
+  for (let i = 0; i < data.length; i++) {
+    if(data[i].indicatorCode === indicatorCode){
+      const keyArr = Object.keys(data[i].data);
+      const valueArr = Object.values(data[i].data);
+      for (let o = 0; o < keyArr.length; o++) {
+        if(valueArr[o] != ""){
+          arrAnio.push(keyArr[o]);
+        }
+      }
+    }
+  }
+
+  return arrAnio;
+}
+
+//listaAnio(data2,"SL.TLF.ADVN.ZS");
+
+// console.log(capturarAnios);
+window.data2 = data2;
+window.listaAnio=listaAnio;
--- src/main.js
+++ src/main.js
@@ -6,19 +6,37 @@ const seleccionPorAnio = document.getElementById("anios");
 const resultado=document.getElementById("resultado-por-año");
 //LLAMANDO A LA FUNCION QUE NOS DA LOS INDICADORCODE - INDICADORNAME
 const indicadores=capturarIndicadores(todoPeruIndicadores);
+// console.log(data2)
 // console.log(indicadores);
+// const aniosSeleccionados=capturarAnios(todoPeruIndicadores);
+// console.log(aniosSeleccionados);
 
 // CREAMOS UNA FUNCION PARA IMPRIMIR EN EL  SELECT LOS INDICATORCODE (VALOR) Y EL INDICATORNAME(NOMBRES DE LOS TEMAS ) 
 const valorSelect = (indicador,elementoIdDom)=>{
-    let string = '<option value="Todos" selected >Todos</option>';
+    let string = '<option value="Todos" selected>Indicadores</option>';
     for(let i=0; i<indicador.length; i++){
-    /*let indicadorId=indicador[i].id);
-    let indicadorName=indicador[i].name);*/
-    string += `<option value=${indicador[i].id}>${indicador[i].name}</option>`
+        /*let indicadorId=indicador[i].id);
+        let indicadorName=indicador[i].name);*/
+        string += `<option value=${indicador[i].id}>${indicador[i].name}</option>`
     }
     elementoIdDom.innerHTML = string;
 };
-
+// LLAMANDO LA FUNCION
 valorSelect(indicadores,seleccionDeIndicadores);
 
+
+// CREAMOS UNA FUNCION PARA IMPRIMIR EN EL  SELECT LOS AÑOS DE CADA INDICE 
+seleccionDeIndicadores.addEventListener( "change" ,(e)=>{
+    const valorSelect= seleccionDeIndicadores.value;
+    const arrAnio = listaAnio(data2,valorSelect);
+
+    let string = '<option value="Todos" selected>Años</option>';
+    for(let i=0; i < arrAnio.length; i++){
+        /*let indicadorId=indicador[i].id);
+        let indicadorName=indicador[i].name);*/
+        string += `<option value=${arrAnio[i]}>${arrAnio[i]}</option>`;
+    }
+
+    seleccionPorAnio.innerHTML = string;
+}) 
 
