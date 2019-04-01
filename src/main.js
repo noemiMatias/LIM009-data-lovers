2ca205f7bcf181b6c5720a929dcde434e99d2eb1
noemimatias <noemi.071213@gmail.com>
2019-04-01 11:37:00 -0500

    juntando cambios

--- src/main.js
+++ src/main.js
@@ -5,36 +5,39 @@ const seleccionPorAnio = document.getElementById("anios");
 const resultado=document.getElementById("resultado-por-año");
 //LLAMANDO A LA FUNCION QUE NOS DA LOS INDICADORCODE - INDICADORNAME
 const indicadores=capturarIndicadores(todoPeruIndicadores);
-// console.log(data2)
 // console.log(indicadores);
-// const aniosSeleccionados=capturarAnios(todoPeruIndicadores);
-// console.log(aniosSeleccionados);
 
 // CREAMOS UNA FUNCION PARA IMPRIMIR EN EL  SELECT LOS INDICATORCODE (VALOR) Y EL INDICATORNAME(NOMBRES DE LOS TEMAS ) 
 const valorSelect = (indicador,elementoIdDom)=>{
-    let string = '<option value="Todos" selected>Indicadores</option>';
+    let string = '<option value="Todos" selected >Todos</option>';
     for(let i=0; i<indicador.length; i++){
-        /*let indicadorId=indicador[i].id);
-        let indicadorName=indicador[i].name);*/
-        string += `<option value=${indicador[i].id}>${indicador[i].name}</option>`
+    /*let indicadorId=indicador[i].id);
+    let indicadorName=indicador[i].name);*/
+    string += `<option value=${indicador[i].id}>${indicador[i].name}</option>`
     }
     elementoIdDom.innerHTML = string;
 };
-// LLAMANDO LA FUNCION
+
 valorSelect(indicadores,seleccionDeIndicadores);
 
+// CAPTURAR EL VALUE DE CADA SELEC CON E-TARGET PARA IMPRIMIR AÑOS
 
-// CREAMOS UNA FUNCION PARA IMPRIMIR EN EL  SELECT LOS AÑOS DE CADA INDICE 
-seleccionDeIndicadores.addEventListener( "change" ,(e)=>{
-    const valorSelect= seleccionDeIndicadores.value;
-    const arrAnio = listaAnio(data2,valorSelect);
+/*seleccionDeIndicadores.addEventListener('change',(e)=>{
+    e.preventDefault(); 
+    const valorTarget= e.target.value;
+    console.log(valorTarget);
+const valorIndice=window.capturarIndice();
+        console.log(window.capturarIndice);
 
-    let string = '<option value="Todos" selected>Años</option>';
-    for(let i=0; i < arrAnio.length; i++){
-        /*let indicadorId=indicador[i].id);
-        let indicadorName=indicador[i].name);*/
-        string += `<option value=${arrAnio[i]}>${arrAnio[i]}</option>`;
-    }
+   const imprimirIndicadores = `<option value=${data[i].indicatorCode}>${data[i].indicatorName}</option>`
 
-    seleccionPorAnio.innerHTML = string;
-}) 
+    
+   seleccionDeIndicadores.innerHTML=imprimirIndicadores;
+
+});
+    
+    };
+
+
+ console.log(e.target.value)*/
+//console.log();
