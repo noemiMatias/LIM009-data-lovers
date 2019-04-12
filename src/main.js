8543577ad5b35773f8b215235c66647fce2cab9d
magalyelguera <elguera.magaly@gmail.com>
2019-04-11 23:58:51 -0500

    hemos testeado todas las funciones exitosamente con excepcion de filtrado y promedio

--- src/main.js
+++ src/main.js
@@ -1,33 +1,34 @@
-const elementoSelectIndicador = document.getElementById("selectores");
-const resultado = document.getElementById("resultado-por-año");
-const selectOrden = document.getElementById("orden");
-const imprimirordenAsc = document.getElementById("resultado-orden-asc");
-const botonPromedio = document.getElementById("promedio-por-select");
-const imprimirPromedio = document.getElementById("resultado-promedio");
+const todoPeru = WORLDBANK['PER']['indicators'];
+const dataTotal = worldbank.todoPeruIndicadores(todoPeru);
+const elementoSelectIndicador = document.getElementById('selectores');
+const resultado = document.getElementById('resultado-por-año');
+const selectOrden = document.getElementById('orden');
+const imprimirordenAsc = document.getElementById('resultado-orden-asc');
+const botonPromedio = document.getElementById('promedio-por-select');
+const imprimirPromedio = document.getElementById('resultado-promedio');
 let arrayFiltrado = [];
 // CREAMOS UNA FUNCION PARA IMPRIMIR EN EL  SELECT LOS INDICATORCODE (VALOR) Y EL INDICATORNAME(NOMBRES DE LOS TEMAS ) 
 const imprimirOpcionesIndicador = (indicadores, elementoIdDom) => {
-    let string = '<option value="Todos" selected>Indicadores</option>';
-    for (let i = 0; i < indicadores.length; i++) {
-        string += `<option value=${indicadores[i].id}>${indicadores[i].name}</option>`
-    }
-    elementoIdDom.innerHTML = string;
+  let string = '<option value="Todos" selected>Indicadores</option>';
+  for (let i = 0; i < indicadores.length; i++) {
+    string += `<option value=${indicadores[i].id}>${indicadores[i].name}</option>`;
+  }
+  elementoIdDom.innerHTML = string;
 };
-//funcion para imprimir selector
-imprimirOpcionesIndicador(capturarIndicadores(todoPeruIndicadores), elementoSelectIndicador);
-elementoSelectIndicador.addEventListener("change", (e) => {
-    const valorSelect = e.target.value;
-    const resultadoAnios = capturarAnios(todoPeruIndicadores);
-    arrayFiltrado = filterData(capturarAnios(todoPeruIndicadores), valorSelect);
-    // console.log(resultadoAnios);
-    let string =  '<table><th scope="col">AÑOS Y VALORES PORCENTUALES</th></table>';
-    for (let i = 0; i < resultadoAnios.length; i++) {
-        if (resultadoAnios[i].indicatorCode === valorSelect) {
-
-            let llaves = Object.keys(resultadoAnios[i].data)
-            for (let j = 0; j < llaves.length; j++) {
-                if (resultadoAnios[i].data[llaves[j]] != "") {
-                    string += `<table class="table table-bordered">
+// funcion para imprimir selector
+imprimirOpcionesIndicador(worldbank.capturarIndicadores(dataTotal), elementoSelectIndicador);
+elementoSelectIndicador.addEventListener('change', (event) => {
+  const valorSelect = event.target.value;
+  const resultadoAnios = worldbank.capturarAnios(dataTotal);
+  arrayFiltrado = worldbank.filterData(worldbank.capturarAnios(dataTotal), valorSelect);
+  // console.log(resultadoAnios);
+  let string = '<table><th scope="col">AÑOS Y VALORES PORCENTUALES</th></table>';
+  for (let i = 0; i < resultadoAnios.length; i++) {
+    if (resultadoAnios[i].indicatorCode === valorSelect) {
+      let llaves = Object.keys(resultadoAnios[i].data);
+      for (let j = 0; j < llaves.length; j++) {
+        if (resultadoAnios[i].data[llaves[j]] !== '') {
+          string += `<table class="table table-bordered">
                     <thead>
                       <tr>
                         <th scope="col">Año</th>
@@ -38,22 +39,22 @@ elementoSelectIndicador.addEventListener("change", (e) => {
                     <th scope="row">${llaves[j]}</th>
                         <td>${resultadoAnios[i].data[llaves[j]]}</td>
                     </tbody>
-                  </table>`
-                }
-            }
-
-            resultado.innerHTML = string;
+                  </table>`;
         }
+      }
+
+      resultado.innerHTML = string;
     }
+  }
 });
 
-selectOrden.addEventListener("change", (e) => {
-    const valorSelect = e.target.value;
-    const newData = ordenadoAscDesc(arrayFiltrado, valorSelect);
-    //console.log(newData)
-    let string = '<table><th scope="col">VALORES PORCENTUALES EN ORDEN ASCENDENTE Y DESCENDENTE</th></table>';
-    for (let i = 0; i < newData.length; i++) {
-        string +=`<table class="table table-bordered">
+selectOrden.addEventListener('change', (event) => {
+  const valorSelect = event.target.value;
+  const newData = worldbank.ordenadoAscDesc(arrayFiltrado, valorSelect);
+  // console.log(newData)
+  let string = '<table><th scope="col">VALORES PORCENTUALES EN ORDEN ASCENDENTE Y DESCENDENTE</th></table>';
+  for (let i = 0; i < newData.length; i++) {
+    string += `<table class="table table-bordered">
                     <thead>
                       <tr>
                         <th scope="col">Año</th>
@@ -62,23 +63,18 @@ selectOrden.addEventListener("change", (e) => {
                     </thead>
                     <tbody>
                     <th scope="row">${newData[i].anio}</th>
-                        <td>${newData[i].val}}</td>
+                        <td>${newData[i].val}</td>
                     </tbody>
-                  </table>`
-
-    }
-    imprimirordenAsc.innerHTML = string
+                  </table>`;
+  }
+  imprimirordenAsc.innerHTML = string;
 });
 
 
-
-
-
-
-botonPromedio.addEventListener("click", () => {
-    const promedio1 = promedio(capturarAnios(todoPeruIndicadores), elementoSelectIndicador.value);
-    let string='<table><th scope="col">PROMEDIO PORCENTUAL</th></table>';;
-    string+=`<table class="table table-bordered">
+botonPromedio.addEventListener('click', () => {
+  const promedio1 = worldbank.promedio(worldbank.capturarAnios(dataTotal), elementoSelectIndicador.value);
+  let string = '<table><th scope="col">PROMEDIO PORCENTUAL</th></table>';
+  string += `<table class="table table-bordered">
     <thead>
       <tr>
         <th scope="col">%</th>
@@ -87,7 +83,7 @@ botonPromedio.addEventListener("click", () => {
     <tbody>
     <th scope="row">${promedio1}</td>
     </tbody>
-  </table>`
+  </table>`;
 
-    imprimirPromedio.innerHTML = string;
+  imprimirPromedio.innerHTML = string;
 });
